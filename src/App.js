7726f00cfc4e5158e166d54730df909f4198d8ce
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-13 06:24:22 +0900

    ドラッグ&ドロップのテスト実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import DragAndDropTest from "./components/DragAndDropTest";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <DragAndDropTest></DragAndDropTest>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DragAndDropTest.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+function DragAndDropTest() {
+  return (
+    <div>
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item0" index={0}>
+                {(provided) => (
+                  <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default DragAndDropTest;

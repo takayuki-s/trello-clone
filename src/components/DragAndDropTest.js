6b20f0e60b88bb5e2c60e32c96c350489bc5d451
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-14 07:36:43 +0900

    ドロップ後の並び替え

--- src/components/DragAndDropTest.js
+++ src/components/DragAndDropTest.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./DragAndDropTest.css";
 
 function DragAndDropTest() {
+  const [items] = useState(["item0", "item1", "item2"]);
+  const onDragEnd = (result) => {
+    // 最初の位置
+    console.log(result.source.index);
+    // 動いた後の位置
+    console.log(result.destination.index);
+    const remove = items.splice(result.source.index, 1);
+    console.log(remove);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -17,10 +27,36 @@ function DragAndDropTest() {
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                   >
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {/* // placeholderエラーになるので以下をお作法として書いておく */}
+              {provided.placeholder}
             </div>
           )}
         </Droppable>

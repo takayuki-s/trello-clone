f003a424202f1a6dcd091c2595b25f0c6b77179f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-16 05:19:48 +0900

    TaskCardTitleコンポーネントの微調整
    ・onBlurで違うところをクリックした時に戻るように
    
    ・autoFocusでクリック時に自動でフォーカスされるように
    
    ・value設定でクリック時に元の文字列から編集ができるように
    
    ・CSS調整

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -17,11 +17,15 @@ export const TaskCardTitle = () => {
     e.preventDefault()
     setIsClick(false)
   }
+
+  const handleBlur = () => {
+    setIsClick(false)
+  }
   return (
-    <div onClick={handleClick}>
+    <div onClick={handleClick} className="taskCardTitleInputArea">
       {isClick ? (
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
+        <input  className="taskCardTitleInput" autoFocus type="text" onChange={handleChange} onBlur={handleBlur} value={inputCardTitle} maxLength="10"/>
       </form>
     ) : (
       <h3>{inputCardTitle}</h3>

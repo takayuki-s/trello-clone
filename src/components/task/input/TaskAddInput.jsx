263f2b3ece5361608c8be921fd5b2274fa69a473
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-19 05:45:45 +0900

    タスク削除後の並び替えのバグ修正

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid';
 
 export const TaskAddInput = ({inputText, setInputText, setTaskList, taskList}) => {
   const handleSubmit = (e) => {
+    const taskId = uuid()
     e.preventDefault()
     // カードを追加する
     if (inputText === "") {
       return
     }
     setTaskList([...taskList, {
-      id: taskList.length,
-      draggableId: `task-${taskList.length}`,
+      id: taskId,
+      draggableId: `task-${taskId}`,
       text: inputText
     }])
     setInputText("")

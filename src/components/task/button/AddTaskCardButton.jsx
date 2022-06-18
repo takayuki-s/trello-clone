c6ddcbd5091346f5a5d73956f8b4e13daf0f6306
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-19 05:56:19 +0900

    AddTaskCardButtonコンポーネント作成

--- src/components/task/button/AddTaskCardButton.jsx
+++ src/components/task/button/AddTaskCardButton.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const AddTaskCardButton = () => {
   return (
-    <div>AddTaskCardButton</div>
+    <div className="addTaskCardButtonArea">
+      <button className="addTaskCardButton">+</button>
+    </div>
   )
 }
